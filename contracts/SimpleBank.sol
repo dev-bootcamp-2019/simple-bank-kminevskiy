a4a2713d8fea36db073e29166a89d3eb90dbd3dc
Konstantin Minevskiy <k@minevskiy.com>
2018-12-18 21:44:31 -0800

    Fix fallback function scope

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 contract SimpleBank {
 
     //
@@ -92,7 +92,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() payable {
+    function() payable external {
         revert();
     }
 }
